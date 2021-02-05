a4ae89e3b6d9a0229db6181a5aea84405cfab99b
replacetr <48390814+replacetr@users.noreply.github.com>
2021-02-05 18:33:42 +0800

    cat sub sub2 ok but no post n save dat

--- assets/react-app/component/product/AddProducts/MCategory.js
+++ assets/react-app/component/product/AddProducts/MCategory.js
@@ -12,12 +12,6 @@ export default class MCategory extends Component {
           categoryname: "",
         },
       ],
-      subcategory : [
-        {
-          subcategoryid : "",
-          subcategoryname : ""
-        }
-      ],
       data: null,
     };
   }
@@ -44,14 +38,14 @@ export default class MCategory extends Component {
   }
 
   handleAddCategory = () => {
-
     this.setState({
-      categories : [
-        ...this.state.categories, {
+      categories: [
+        ...this.state.categories,
+        {
           categoryid: "",
           categoryname: "",
-        }
-      ]
+        },
+      ],
     });
   };
 
@@ -65,21 +59,20 @@ export default class MCategory extends Component {
     );
   };
 
-
   handleInput = (i) => (e) => {
     console.log(e.target.value);
     const data = this.state.data.find(
       (datas, i) => e.target.value === datas.name
     );
-    console.log("kena cari",data);
+    console.log("kena cari", data);
 
     const newData = this.state.categories.map((post, idx) => {
       if (i !== idx) return post;
       if (data !== undefined) {
-          //this return value if select
+        //this return value if select
         return { ...post, categoryname: data.name, categoryid: data.id };
       } else {
-          //this return new value without select
+        //this return new value without select
         return { ...post, categoryname: e.target.value, categoryid: null };
       }
     });
@@ -87,37 +80,83 @@ export default class MCategory extends Component {
     this.setState({
       categories: newData,
     });
-    this.props.changecategory(newData)
+    this.props.changecategory(newData);
+  };
 
-  }
-
-  handleAddSubCategory =(index) =>{
-    const addsub = this.state.categories.map((cat,cati)=>{
+  handleAddSubCategory = (index) => {
+    const addsub = this.state.categories.map((cat, cati) => {
       if (index !== cati) return cat;
-      return {...cat,subcategory : [{subcategoryid: "", subcategoryname : ""}]  }
+      return {
+        ...cat,
+        subcategory: [{ subcategoryid: "", subcategoryname: "" }],
+      };
+    });
+    console.log("add sub", addsub);
+    this.setState({ categories: addsub });
+  };
 
-    })
-    console.log("add sub",addsub)
-    this.setState({categories : addsub})
-
-  }
-
-  handleAddSubCategoryTwo = (i) =>{
-    console.log("init addsub2",this.state.categories);
-    const addsubtwo = this.state.categories.map((addsubtwocat,itwo)=>{
+  handleAddSubCategoryTwo = (i) => {
+    console.log("init addsub2", this.state.categories);
+    const addsubtwo = this.state.categories.map((addsubtwocat, itwo) => {
       if (i !== itwo) return addsubtwocat;
-      addsubtwocat.subcategory.map((datasub,idxs)=> {
-        return {...datasub , subcategorytwo :[{subcategoryid: "", subcategoryname : ""}] }
-      })
-      
-
-    })
+      return {
+        ...addsubtwocat,
+        subcategory: addsubtwocat.subcategory.map((datasub, idxsa) => {
+          return {
+            ...datasub,
+            subcategorytwo: [{ subcategorytwoid: "", subcategorytwoname: "" }],
+          };
+        }),
+      };
+    });
     console.log("add sub two", addsubtwo);
     this.setState({
-      categories : addsubtwo
-    })
-    
-  }
+      categories: addsubtwo,
+    });
+  };
+
+  handleSubInput = (catidx, subidx) => (e) => {
+    // const catsel = this.state.categories.find((cat,ind)=>{x===ind})
+    const modified = this.state.categories.map((mod, modindx) => {
+      if (catidx !== modindx) return mod;
+      return {
+        ...mod,
+        subcategory: mod.subcategory.map((sub, isub) => {
+          if (subidx !== isub) return sub;
+          return { ...sub, subcategoryname: e.target.value, subcategoryid: "" };
+        }),
+      };
+    });
+
+    console.log("modified :", modified);
+    this.setState({
+      categories: modified,
+    });
+  };
+
+  handleSubTwoInput = (incatindx, incatsubidx, incatsubtwoidx) => (event) => {
+    const subtwonew = this.state.categories.map((catsubtwo, catsubtwoidx) => {
+      if (incatindx !== catsubtwoidx) return catsubtwo;
+      return {
+        ...catsubtwo,
+        subcategory: catsubtwo.subcategory.map((catsub, catsubidx) => {
+          if (incatsubidx !== catsubidx) return catsub;
+          return {
+            ...catsub,
+            subcategorytwo: catsub.subcategorytwo.map((subtwo, subtwoidx) => {
+              if (incatsubtwoidx !== subtwoidx) return subtwo;
+              return { ...subtwo, subcategorytwoname: event.target.value };
+            }),
+          };
+        }),
+      };
+    });
+    console.log("subtwonew cat :", subtwonew);
+    this.setState({
+      categories: subtwonew,
+    });
+  };
+
   render() {
     console.log(this.state);
     return (
@@ -142,44 +181,59 @@ export default class MCategory extends Component {
               className="btn btn-primary btn-sm p-2 m-2"
             >
               X
-            </a> 
+            </a>
 
-            {data.subcategory ? data.subcategory.map((sub,idx)=>(
-              <div key={idx}>
-                <input type="text" size="12" placeholder={`Sub Category#${idx + 1}`}/>
+            {data.subcategory ? (
+              data.subcategory.map((sub, subidx) => (
+                <div key={subidx}>
+                  <input
+                    type="text"
+                    size="12"
+                    placeholder={`Sub Category#${subidx + 1}`}
+                    onInput={this.handleSubInput(index, subidx)}
+                  />
 
-                {sub.subcategorytwo ? sub.subcategorytwo.map((sub2,ind2)=>(
-                  <div key={ind2}>
-                    <input type="text" size="12" placeholder={`Sub Two Category#${indtwo + 1}`}/>
-                  </div>
-                ))
-                  
-                 : <a
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              this.handleAddSubCategoryTwo(index);
-            }}
-            className="btn btn-primary btn-sm p-2 m-2"
-          >
-            Add Sub Category Two
-          </a>}
-                
-
-
-
-              </div>
-
-            )): <a
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              this.handleAddSubCategory(index);
-            }}
-            className="btn btn-primary btn-sm p-2 m-2"
-          >
-            Add Sub Category
-          </a>}
+                  {sub.subcategorytwo ? (
+                    sub.subcategorytwo.map((sub2, indtwo) => (
+                      <div key={indtwo}>
+                        <input
+                          type="text"
+                          size="12"
+                          placeholder={`Sub Two Category#${indtwo + 1}`}
+                          onInput={this.handleSubTwoInput(
+                            index,
+                            subidx,
+                            indtwo
+                          )}
+                        />
+                      </div>
+                    ))
+                  ) : (
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        this.handleAddSubCategoryTwo(index);
+                      }}
+                      className="btn btn-primary btn-sm p-2 m-2"
+                    >
+                      Add Sub Category Two
+                    </a>
+                  )}
+                </div>
+              ))
+            ) : (
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.handleAddSubCategory(index);
+                }}
+                className="btn btn-primary btn-sm p-2 m-2"
+              >
+                Add Sub Category
+              </a>
+            )}
           </div>
         ))}
         <datalist id="data">
@@ -199,8 +253,6 @@ export default class MCategory extends Component {
         >
           Add Category
         </a>
-
-        
       </div>
     );
   }
