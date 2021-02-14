b9f810acad4ccf35ad670c81a6de18d60d6bd380
replacetr <48390814+replacetr@users.noreply.github.com>
2021-02-15 06:52:27 +0800

    rusak

--- assets/react-app/component/Visitor/Visitor.js
+++ assets/react-app/component/Visitor/Visitor.js
@@ -1,17 +1,111 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { setDefaultLocale } from "react-datepicker";
+import { Table } from "react-bootstrap";
 
 export default function Visitor() {
   const [data, setData] = useState(null);
+  const [table, setTable] = useState(null);
+
   useEffect(() => {
     axios
       .get("api/admin/test/test")
-      .then((res) => setData(res.data))
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
       .catch((e) => console.log(e));
   }, []);
 
-  console.log(data);
+  function countObjs(arr) {
+    // So the object doesn't inherit from Object.prototype and avoids property
+    // name collisions
+    var obj = Object.create(null);
+    arr.forEach(function (item) {
+      if (obj[item]) {
+        obj[item]++;
+      } else {
+        obj[item] = 1;
+      }
+    });
+    return obj;
+  }
 
-  return <div>kambing</div>;
+  const selectOnChange = (e) => {
+    const target = e.target.value;
+    if (target === "Country") {
+      const country = data.map((ind) => {
+        return ind.Country;
+      });
+      const countCountry = countObjs(country);
+
+      setTable({ Country: countCountry });
+    } else if (target === "State") {
+      const state = data.map((ind) => {
+        return ind.state;
+      });
+
+      const cstate = countObjs(state);
+      setTable({ State: cstate });
+    } else if (target === "City") {
+      const city = data.map((ind) => ind.city);
+      const ccity = countObjs(city);
+      setTable({ City: ccity });
+    } else if (target === "IP") {
+    }
+  };
+
+  if (data) {
+    const state = data.map((ind) => {
+      return ind.state;
+    });
+    const Country = data.map((ind) => {
+      return ind.Country;
+    });
+    const visit = countObjs(state);
+    const singledata = data[0];
+    const name = Object.getOwnPropertyNames(singledata);
+    console.log("name", name);
+  }
+
+  console.log(table);
+
+  return (
+    <div className="container">
+      <div className="mt-3">
+        {data && <h3>Total Visitor : {data.length}</h3>}
+
+        <select
+          defaultValue={{ label: 2002, value: 2002 }}
+          onChange={selectOnChange}
+        >
+          <option value="Country">Country</option>
+          <option value="State">State</option>
+          <option value="City">City</option>
+          <option value="IP">IP</option>
+        </select>
+
+        {table && (
+          <div>
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>{Object.keys(table)[0]}</th>
+                  <th>Table</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  {table[Object.keys(table)[0]].map((x, i) => {
+                    return <th>{x[Object.keys(x)[i]]}</th>;
+                  })}
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        )}
+
+        <div></div>
+      </div>
+    </div>
+  );
 }
